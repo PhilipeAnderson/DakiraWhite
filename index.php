090697a5ba19040730ce56525b07761375abdff3
philipe_kraken <philipeacampos@hotmail.com>
2022-04-25 09:52:53 -0300

    init web page

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+        <head>
+                <meta charset="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="description" content="www web www" />
+                <link href="styles/style.css" rel="stylesheet" />
+                <title>Akira White</title>
+        </head>
+        <body>
+                <h1>Dakira White</h1>
+
+
+
+
+
+
+
+
+                <script src="scripts/script.js" type="text/javascript"></script>
+        </body>
+</html>
+
