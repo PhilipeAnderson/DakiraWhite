321582ebd6320f607059d15b6b60496d4429db1c
philipe_kraken <philipeacampos@hotmail.com>
2022-04-28 11:55:45 -0300

    changed architecture of the folders

--- index.php
+++ index.php
@@ -3,59 +3,60 @@ $pages = ['home'=>'Home Page', 'links'=>'Links Page', 'about'=> 'About Page', 'S
 ?>
 <!DOCTYPE html>
 <html lang="en">
-        <head>
-                <meta charset="utf-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <meta name="description" content="www web www" />
-                <link href="styles/style.css" rel="stylesheet" />
-                <title>Akira White</title>
-        </head>
-        <body>
-		<header class="header">
-			<?php foreach ($pages as $key => $value){ 
-				echo '<a href="?page='.$key.'">'.ucfirst($key).'</a>';
-				} 
-			 ?>
-		</header> 
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="description" content="www web www" />
+		<link href="styles/style.css" rel="stylesheet" />
+		<title> Dakira White </title>
+	</head>
+	
+	<body>
+	<header class="header">
+	<?php foreach ($pages as $key => $value){ 
+		echo '<a href="?page='.$key.'">'.ucfirst($key).'</a>';
+		} 
+		?>
+	</header> 
+
+	<main class="main" align="center">
+	<h2> List Sites .onion </h2>
+
+	<section>
+		<h3>
+			<?php$page=(isset($_GET['page'])?$_GET['page']:'home');
+
+			if(!array_key_exists($page, $pages)){
+				$page = 'home'
+			}
 		
-		<main class="main" align="center">
-			<h2> List Sites .onion </h2>
+			echo ucfirst($page);
 
-			<section>
-				<h3>
-					<?php$page=(isset($_GET['page'])?$_GET['page']:'home');
+			?>
+		</h3>
+		<p>
+			<!--?php echo $pages[$page];  ?-->
+			Cotent dinamic	
+		</p>	
+	</section>
 
-					if(!array_key_exists($page, $pages)){
-						$page = 'home'
-					}
-				
-					echo ucfirst($page);
+	<ul align="center">
+		<li><a href="#">Test 1</a></li>
+		<li><a href="#">Test 2</a></li>
+		<li><a href="#">Test 3</a></li>
+		<li><a href="#">Test 4</a></li>
+		<li><a href="#">Test 5</a></li>
+		<li><a href="#">Test 6</a></li>
+		<li><a href="#">Test 7</a></li>
+	</ul>
+	</main>
 
-					?>
-				</h3>
-				<p>
-					<!--?php echo $pages[$page];  ?-->
-					Cotent dinamic	
-				</p>	
-			</section>
-
-			<ul align="center">
-				<li><a href="#">Test 1</a></li>
-				<li><a href="#">Test 2</a></li>
-				<li><a href="#">Test 3</a></li>
-				<li><a href="#">Test 4</a></li>
-				<li><a href="#">Test 5</a></li>
-				<li><a href="#">Test 6</a></li>
-				<li><a href="#">Test 7</a></li>
-			</ul>
-		</main>
-
-		<footer class="footer">
-			<div class="Logo"><img src="./images/logo.png" width="150px" /></div>
-			<div class="Central"><h2>Thanks for visit me</h2></div>
-			<div class="DuckGo"><a href="#"><img src="./images/duckGo.png" width="150px" alt="Image Duck Duck Go" /></a></div>
-		</footer>
-                <script src="scripts/script.js" type="text/javascript"></script>
-        </body>
+	<footer class="footer">
+	<div class="Logo"><img src="./images/logo.png" width="150px" /></div>
+	<div class="Central"><h2>Thanks for visit me</h2></div>
+	<div class="DuckGo"><a href="#"><img src="./images/duckGo.png" width="150px" alt="Image Duck Duck Go" /></a></div>
+	</footer>
+	<script src="scripts/script.js" type="text/javascript"></script>
+	</body>
 </html>
 
---
+++ src/brigadier/Foot/index.php
@@ -0,0 +1,3 @@
+    <sript type="text/javascript" src="../../../scripts/script.js"></sript>
+  </body>
+</html>
